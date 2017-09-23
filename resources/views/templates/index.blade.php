43bf642f0ee5a89ba3914af0f3440eaa634e81d6
Artenes Nogueira <artenesama@gmail.com>
2017-09-23 16:01:07 -0300

    Add pagination and search bar

--- resources/views/templates/index.blade.php
+++ resources/views/templates/index.blade.php
@@ -10,6 +10,15 @@
 
     </div>
 
+    <div class="field search-field">
+        <p class="control has-icons-left has-icons-right">
+            <input class="input is-normal" type="text" placeholder="search for themes">
+            <span class="icon is-small is-left">
+      <i class="fa fa-search"></i>
+    </span>
+        </p>
+    </div>
+
     @foreach($templates->chunk(3) as $templateSet)
 
         <div class="columns">
@@ -21,7 +30,9 @@
                     <div class="card">
 
                         <header class="card-header">
-                            <p class="card-header-title template-name"><a href="{{ route('demos.show', $template->slug) }}" target="_blank">{{ $template->name }}</a></p>
+                            <p class="card-header-title template-name"><a
+                                        href="{{ route('demos.show', $template->slug) }}"
+                                        target="_blank">{{ $template->name }}</a></p>
                         </header>
 
                         <div class="card-image">
@@ -33,7 +44,9 @@
                         </div>
 
                         <header class="card-header">
-                                <p class="card-header-title template-title"><a href="{{ route('demos.show', $template->slug) }}" target="_blank">{{ $template->title }}</a></p>
+                            <p class="card-header-title template-title"><a
+                                        href="{{ route('demos.show', $template->slug) }}"
+                                        target="_blank">{{ $template->title }}</a></p>
                         </header>
 
                         <div class="other-store-price">
@@ -46,16 +59,18 @@
                             <div class="has-text-centered our-price">
 
                                 <p class="is-size-7">for just</p>
-                                <p class="is-size-3"><span class="icon"><i class="fa fa-dollar"></i>{{ $template->price }}</span></p>
+                                <p class="is-size-3"><span class="icon"><i
+                                                class="fa fa-dollar"></i>{{ $template->price }}</span></p>
 
                             </div>
 
                         </div>
 
                         <footer class="card-footer">
-                            <a href="{{ route('demos.show', $template->slug) }}" target="_blank" class="card-footer-item footer-item footer-item-demo">demo</a>
-                            <a href="{{ route('templates.show', $template->slug) }}" target="_blank" class="card-footer-item">info</a>
-                            <a href="{{ route('checkout.show', $template->slug) }}" class="card-footer-item footer-item footer-item-bruy">bruy it</a>
+                            <a href="{{ route('demos.show', $template->slug) }}" target="_blank"
+                               class="card-footer-item footer-item footer-item-demo">demo</a>
+                            <a href="{{ route('checkout.show', $template->slug) }}"
+                               class="card-footer-item footer-item footer-item-bruy">bruy it</a>
                         </footer>
 
                     </div>
@@ -66,5 +81,19 @@
         </div>
 
     @endforeach
+
+    <nav class="pagination is-centered" role="navigation" aria-label="pagination">
+        <a class="pagination-previous">Previous</a>
+        <a class="pagination-next">Next page</a>
+        <ul class="pagination-list">
+            <li><a class="pagination-link" aria-label="Goto page 1">1</a></li>
+            <li><span class="pagination-ellipsis">&hellip;</span></li>
+            <li><a class="pagination-link" aria-label="Goto page 45">45</a></li>
+            <li><a class="pagination-link is-current" aria-label="Page 46" aria-current="page">46</a></li>
+            <li><a class="pagination-link" aria-label="Goto page 47">47</a></li>
+            <li><span class="pagination-ellipsis">&hellip;</span></li>
+            <li><a class="pagination-link" aria-label="Goto page 86">86</a></li>
+        </ul>
+    </nav>
 
 @endsection
