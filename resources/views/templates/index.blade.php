9f7c480f00950f5e9ec374d78d8867a827802733
Artenes Nogueira <artenesama@gmail.com>
2017-09-25 03:26:12 +0000

    Remove duplicate footer and change header text

--- resources/views/templates/index.blade.php
+++ resources/views/templates/index.blade.php
@@ -5,10 +5,10 @@
     <div class="hero-body with-black-pattern">
         <div class="container has-text-centered">
             <h1 class="title main-title">
-                The chepeast templates on the internet
+                The chepeast premium templates on the internet
             </h1>
             <h2 class="subtitle">
-                premium templates for half the price of other stores
+                premium templates for less than half the price of other stores
             </h2>
         </div>
     </div>
@@ -26,23 +26,6 @@
         </div>
 
     </div>
-
-    <footer class="footer">
-        <div class="container">
-            <div class="content has-text-centered">
-                <p>
-                    <strong>Bulma</strong> by <a href="http://jgthms.com">Jeremy Thomas</a>. The source code is licensed
-                    <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-                    is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
-                </p>
-                <p>
-                    <a class="icon" href="https://github.com/jgthms/bulma">
-                        <i class="fa fa-github"></i>
-                    </a>
-                </p>
-            </div>
-        </div>
-    </footer>
 
     <script type="text/javascript" src="{{ asset('js/search.js') }}"></script>
 
